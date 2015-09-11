de44d4f76a3346128109259ec5e8735c933ab2f2
Pavel Losev <inky.thousand@gmail.com>
2015-09-11 22:07:37 +0300

    third commit

--- indexgit.php
+++ indexgit.php
@@ -8,6 +8,6 @@
 	<h1>Git Hello</h1>
 	<p>First commit</p>
 	<p>Second changes</p>
-	<p>Save the elephant</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, molestiae, omnis architecto ad ratione consectetur eius. Saepe, quia in minus explicabo repudiandae laboriosam doloribus sint. Consectetur, voluptas perspiciatis mollitia maiores.</p>
 </body>
 </html>
